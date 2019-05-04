72e3864b070d4cfb6e297d62645da9bff53cc68d
xuanh251 <lexuanhoang1997@gmail.com>
2019-05-04 08:45:23 +0700

    ud3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,30 @@
 export const environment = {
-  production: true
+  production: true,
+  ApiOriginUrl: 'http://1dcdc3bd.ngrok.io',
+  apiUrl: 'http://1dcdc3bd.ngrok.io/api/',
+  AppName: 'HỆ THỐNG QUẢN LÍ THI ĐUA KHEN THƯỞNG TRƯỜNG ĐẠI HỌC QUẢNG NAM',
+  dataTableLanguage: {
+    'sProcessing':    'Đang tải...',
+    'sLengthMenu':    'Hiển thị _MENU_ bản ghi trên 1 trang',
+    'sZeroRecords':   'Không có dữ liệu',
+    'sEmptyTable':    'Không có dữ liệu',
+    'sInfo':          'Dữ liệu hiển thị từ _START_ đến _END_ trong tổng số _TOTAL_ bản ghi',
+    'sInfoEmpty':     'Dữ liệu hiển thị từ 0 đến 0 trong tổng số 0 bản ghi',
+    'sInfoFiltered':  '(Dữ liệu lọc trong tổng số _MAX_ bản ghi)',
+    'sInfoPostFix':   '',
+    'sSearch':        'Tìm kiếm:',
+    'sUrl':           '',
+    'sInfoThousands':  ',',
+    'sLoadingRecords': 'Đang tải...',
+    'oPaginate': {
+        'sFirst':    'Đầu',
+        'sLast':    'Cuối',
+        'sNext':    'Tiếp',
+        'sPrevious': 'Trước'
+    },
+    'oAria': {
+        'sSortAscending':  ': Đang chọn lọc dữ liệu xuôi',
+        'sSortDescending': ': Đang chọn lọc dữ liệu ngược'
+    }
+  }
 };
